3782f6ac63b38ad9bd4cc3a699337b8b78a18ed8
narendraperakas-beep <narendraperakas@gmail.com>
2026-09-22 08:47:52 +0700

    Add files via upload

---
+++ AritmatikaPenjumlahan/AritmatikaPenjumblahan/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AritmatikaPenjumblahan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int mangga, appel, hasil = 0;
+
+            Console.WriteLine("mangga = ");
+            mangga = int.Parse(Console.ReadLine());
+            Console.Write("appel = ");
+            appel = int.Parse(Console.ReadLine());
+
+            // operasi penjumblahan dengan operator +
+            hasil = mangga + appel;
+
+            Console.WriteLine($"Hasil mangga + appel = {hasil}");
+        }
+    }
+}
